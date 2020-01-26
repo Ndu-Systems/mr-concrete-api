c4d125048a4eec63f2298e5fb13bfde71be9ad35
Freedom Khanyile <Freedom.Khanyile@betterlife.co.za>
2020-01-26 19:57:11 +0200

    Update sign in to not return password.

--- api/users/sign-in.php
+++ api/users/sign-in.php
@@ -4,8 +4,8 @@ include_once '../../models/Users.php';
 
 $data = json_decode(file_get_contents("php://input"));
 
-$Email = $data->email;
-$Password = $data->password; 
+$Email = $data->Email;
+$Password = $data->Password; 
 
 $database = new Database();
 $db = $database->connect();
@@ -13,5 +13,6 @@ $db = $database->connect();
 $user = new Users($db);
 
 $result = $user->getUserByEmailAndPassword($Email,$Password);
+$result['Password'] = null;
 
 echo json_encode($result);
