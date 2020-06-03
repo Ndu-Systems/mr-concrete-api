d848e4a645b67e07df9523a139398d1a8d9fb91c
Freedom Khanyile <Freedom.Khanyile@betterlife.co.za>
2020-06-03 23:05:32 +0200

    remove unused code.

---
+++ api/users/add-user.php
@@ -0,0 +1,47 @@
+<?php
+include_once '../../config/Database.php';
+include_once '../../models/Users.php';
+include_once '../../models/Roles.php';
+
+$data = json_decode(file_get_contents("php://input"));
+
+$Email = $data->Email;
+$Password = 12345; // default password (flow required to change on first login)
+$FirstName = $data->FirstName;
+$LastName = $data->LastName;
+$UserType = $data->TypeOfUser;
+$CreateUserId = $data->CreateUserId;
+$ModifyUserId = $data->ModifyUserId;
+$Cellphone =  $data->Cellphone;
+$StatusId = 9; // Awaiting Activation
+
+
+$database = new Database();
+$db = $database->connect();
+
+$users = new Users($db);
+$roles = new Roles($db);
+
+$roleResult = $roles->getByRoleName($UserType);
+$RoleId = $roleResult['Id'];
+
+$UserId = $database->getGuid($db);
+$result = $users->AddUser(
+    $UserId,
+    $FirstName,
+    $LastName,
+    $Email,
+    $Password,
+    $Cellphone,
+    $RoleId,
+    $UserType,
+    $CreateUserId,
+    $ModifyUserId,
+    $StatusId
+);
+
+if ($result) {
+    echo json_encode($result);
+} else {
+    echo json_encode('internal error');
+} 
